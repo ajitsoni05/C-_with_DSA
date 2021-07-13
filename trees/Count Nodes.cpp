20b8e83adafedc4490fa45d3b149df62fcd9ed38
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-07-14 04:31:50 +0530

    Update Count Nodes.cpp

--- trees/Count Nodes.cpp
+++ trees/Count Nodes.cpp
@@ -1,3 +1,24 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+(
+************************************************************/
 int numNodes(TreeNode<int>*root)
 {
 //Edge case
