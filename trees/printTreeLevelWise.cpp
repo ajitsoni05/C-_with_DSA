a850ed3afd6c8ab32a218b2a7e3abf54552421e4
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-07-14 04:15:31 +0530

    Create printTreeLevelWise.cpp

---
+++ trees/printTreeLevelWise.cpp
@@ -0,0 +1,52 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+(
+************************************************************/
+#include<queue>
+void printLevelWise(TreeNode<int>* root)
+{
+    if(root==NULL)
+    {
+        return ;
+    }
+    queue<TreeNode<int>*>pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size()!=0)
+    {
+        TreeNode<int>* front=pendingNodes.front();
+        pendingNodes.pop();
+        cout<<front->data<<":";
+        int numchild=front->children.size();
+        for(int i=0;i<numchild;i++)
+        { 
+            if(i==numchild-1)
+        {
+            cout<<front->children[i]->data;
+            
+        }else{
+            cout<<front->children[i]->data<<",";
+            }
+            pendingNodes.push(front->children[i]);
+        }
+        cout<<endl;
+        
+    }
+}
