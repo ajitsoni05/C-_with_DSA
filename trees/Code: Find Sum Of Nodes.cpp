7bef951fbc8a70bb6e56eefe409cc96d93553e29
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-07-14 04:33:24 +0530

    Create Code: Find Sum Of Nodes.cpp

---
+++ trees/Code: Find Sum Of Nodes.cpp
@@ -0,0 +1,32 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+int sumOfNodes(TreeNode<int>* root) {
+    // Write your code here
+    int ans=root->data;
+    for(int i=0;i<root->children.size();i++)
+    {
+        ans+=sumOfNodes(root->children[i]);
+    }
+    return ans;
+}
