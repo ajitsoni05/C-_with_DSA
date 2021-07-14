a2b4f9539a6db472cf710f27073bf91439888edd
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-07-15 03:22:53 +0530

    Update Contains X.cpp

--- trees/Assignment/Contains X.cpp
+++ trees/Assignment/Contains X.cpp
@@ -1,1 +1,43 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
 
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+bool isPresent(TreeNode<int>* root, int x) {
+    // Write your code here
+    if(root==NULL)
+    {
+     return false;
+    }
+    if(root->data==x)
+    {
+        return true;
+    }
+    for(int i=0;i<root->children.size();i++)
+    {
+        bool small=isPresent(root->children[i],x);
+       if(small==true)
+       {
+           return true;
+       }
+    }
+    return false;
+}
