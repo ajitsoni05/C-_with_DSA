d5193707a5cd0ecac05da56a4ca7e1c4ff7c4ac9
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-07-15 02:41:27 +0530

    Create PreOrder.cpp

---
+++ trees/PreOrder.cpp
@@ -0,0 +1,35 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+void preorder(TreeNode<int>*root)
+{
+	//Edge case if root is NULL
+	if(root==NULL)
+	{
+		return;
+	}
+	cout<<root->data<<" ";
+	for(int i=0;i<root->children.size();i++)
+	{
+		preorder(root->children[i]);
+	}
+}
