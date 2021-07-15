c4b2c769b8d9a19f0434b090920bad23a1e9b931
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-07-16 03:04:02 +0530

    Create Structurally Identical.cpp

---
+++ trees/Assignment/Structurally Identical.cpp
@@ -0,0 +1,25 @@
+/*
+Time complexity: O(N)
+Space complexity: O(H)
+where N is the number of nodes in the smaller tree and
+H is the height/depth of the smaller tree
+*/
+bool areIdentical(TreeNode<int> *root1, TreeNode<int> *root2) {
+if (root1 == NULL && root2 == NULL) {
+return true;
+}
+if ((root1 == NULL && root2 != NULL) || (root1 != NULL && root2 == NULL)) {
+return false;
+}
+if ((root1->data != root2->data) || (root1->children.size() != root2->children.size())) {
+return false;
+}
+for (int i = 0; i < root1->children.size(); ++i) {
+TreeNode<int> *child1 = root1->children[i];
+TreeNode<int> *child2 = root2->children[i];
+if (!areIdentical(child1, child2)) {
+return false;
+}
+}
+return true;
+}
