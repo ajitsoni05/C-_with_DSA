da1c4339d0e80358c56778ad84950dfb39e03a8b
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-07-16 03:31:19 +0530

    Create NextLarger.cpp

---
+++ trees/Assignment/NextLarger.cpp
@@ -0,0 +1,53 @@
+/*
+Time complexity: O(N)
+Space complexity: O(H)
+where N is the number of nodes in the tree and
+H is the height/depth of the tree
+*/
+/************************************************************
+
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+
+        TreeNode(T data) {
+            this->data = data;
+        }
+
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+TreeNode<int>* getNextLargerElement(TreeNode<int>* root, int x) {
+    // Write your code here
+    TreeNode<int>* ans=NULL;
+    if(root==NULL){
+        return ans;
+    }
+    if(root->data>x)
+    {
+        ans=root;
+    }
+    for(int i=0;i<root->children.size();i++)
+    {
+        TreeNode<int>*small=getNextLargerElement(root->children[i],x);
+        if(small!=NULL)
+        {
+            if(ans==NULL||ans->data>small->data)
+            {
+                ans=small;
+            }
+        }
+    }
+
+    return ans;
+}
